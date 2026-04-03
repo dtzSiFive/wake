177f1cb38c19edb67897bb8d99fe095286f6657e
Will Dietz <will.dietz@sifive.com>
2026-04-03 13:32:04 -0700

    hardlink: fail.sh, stderr

--- tests/wakebox/hardlink/fail.sh
+++ tests/wakebox/hardlink/fail.sh
@@ -3,7 +3,7 @@
 # So we fill PATH with some typical values.
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin
 
-trap 'rm link_file_src.txt link_file_dst.txt' EXIT
+trap 'rm -f link_file_src.txt link_file_dst.txt' EXIT
 
 STDOUT=$(${1}/wakebox -p input.json)
 
